abad156dbff749695ac295f80c18a449c2d18975
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-04-12 10:50:33 -0300

    se agrego una suma de 2 numero por medio de inputs y se mostro el resultado por pantalla

---
+++ js/05-SumaGeneralizada.js
@@ -0,0 +1,5 @@
+/* Escribe un programa de tres líneas que pida un número, pida 
+otro número y escriba el resultado de sumar estos dos números. */
+let numOne = parseInt(prompt("Dime un numero:"))
+let numTwo = parseInt(prompt("Dime otro numero"))
+document.writeln("La suma de los numeros es: ", numOne + numTwo)
