16cbb3bb3073d2c632020b6ecbc229af455a8e05
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-04-14 19:06:09 -0300

    se agrego el ejercicio 3 de los que habia que hacer con PSEint (multiplicar x10 un numero)

---
+++ js/14-Multiplicarx10PSEint.js
@@ -0,0 +1,4 @@
+/* Multiplicar un número por 10:  Crear un algoritmo que permita al usuario 
+ingresar un número y luego mostrar el resultado de multiplicarlo por 10. */
+let numOne = parseInt(prompt("Ingrese un numero:"))
+document.writeln("Multiplicado x10 es: ", numOne*10)
